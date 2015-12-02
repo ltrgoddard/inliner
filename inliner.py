e2def5902f6f207eeaf2aa7859f9b5d8e63b60a5
ltrgoddard <louisgoddard@gmail.com>
2015-12-02 15:31:02 +0000

    Update inliner.py
    Added support for citations at the end of paragraphs.

--- inliner.py
+++ inliner.py
@@ -9,6 +9,7 @@
 #        [number of notes in file]
 
 import sys
+import re
 
 with open(str(sys.argv[1]),"r") as input:
   text = input.readlines()
@@ -21,6 +22,7 @@ while counter <= int(sys.argv[3]):
 
   ref = "[^" + str(counter) + "]:"
   cite = "[^" + str(counter) + "] "
+  cite2 = "[^" + str(counter) + "]\n"
   matching = [s for s in text if ref in s]
   
   if counter < 10:
@@ -31,7 +33,10 @@ while counter <= int(sys.argv[3]):
     note = "^[" + str(matching)[10:-4] + "] "
 
   final = final.replace(cite, note)
+  final = final.replace(cite2, note + "\n")
 
+  print counter
+  print note
   counter = counter + 1
 
 offcut = final.find("[^1]:")
