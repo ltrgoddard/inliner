6db07086184d387874113470f3f4c426df6e18e4
ltrgoddard <louisgoddard@gmail.com>
2016-03-05 17:56:44 +0000

    replaced two-space indents with four-space indents

--- inliner.py
+++ inliner.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # inliner - converts reference-style Markdown endnotes to
-#           Pandoc Markdown's inline footnotes
+# Pandoc Markdown's inline footnotes
 
 # by Louis Goddard <louisgoddard@gmail.com>
 
@@ -10,38 +10,38 @@
 import sys
 
 with open(str(sys.argv[1]),"r") as input:
-  text = input.read()
+    text = input.read()
 
 counter = 0
 
 while True:
-  try:
+    try:
 
-    counter = counter + 1
+        counter = counter + 1
 
-    ref = "[^" + str(counter) + "]:"
-    nextRef = "[^" + str(counter + 1) + "]:"
-    cite = "[^" + str(counter) + "]"
+        ref = "[^" + str(counter) + "]:"
+        nextRef = "[^" + str(counter + 1) + "]:"
+        cite = "[^" + str(counter) + "]"
 
-    refStart = text.index(ref)
-    refEnd = text.index(nextRef) - 2
-  
-    if counter <= 9:
-      offset = 6
-    elif counter >= 10 and counter <= 100:
-      offset = 7
-    elif counter >= 100 and counter <= 1000:
-      offset = 8
-    else:
-      offset = 9
+        refStart = text.index(ref)
+        refEnd = text.index(nextRef) - 2
+    
+        if counter <= 9:
+            offset = 6
+        elif counter >= 10 and counter <= 100:
+            offset = 7
+        elif counter >= 100 and counter <= 1000:
+            offset = 8
+        else:
+            offset = 9
 
-    note = "^[" + text[refStart+offset:refEnd] + "]"
-    text = text.replace(cite, note)
+        note = "^[" + text[refStart+offset:refEnd] + "]"
+        text = text.replace(cite, note)
 
-  except:
+    except:
 
-    break
-  
+        break
+    
 note = "^[" + text[refStart+offset:len(text)-1] + "]"
 text = text.replace(cite, note)
 
@@ -49,4 +49,4 @@ cutPoint = text.index("\n^")
 text = text[0:cutPoint]
 
 with open(str(sys.argv[2]),"w") as output:
-  output.write(text)
+    output.write(text)
